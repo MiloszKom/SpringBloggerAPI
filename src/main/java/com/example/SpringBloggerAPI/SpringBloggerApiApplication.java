44f574da5946f4f92fb89b5cf6a1ae7421dda776
MiloszK <milosz.kom@onet.pl>
2025-07-12 10:11:39 +0200

    Initial commit

---
+++ src/main/java/com/example/SpringBloggerAPI/SpringBloggerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBloggerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBloggerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBloggerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBloggerAPI/SpringBloggerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBloggerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBloggerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
